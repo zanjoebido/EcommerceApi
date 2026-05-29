1abd2689961fb48b0b22e457d65e4945ec758356
zanjoebido <zanjoebido@gmail.com>
2026-05-29 16:53:10 +0800

    chore: initial spring boot bootstrap structure from initializr

---
+++ src/test/java/com/ws101/bido/EcommerceApi/EcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ws101.bido.EcommerceApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
